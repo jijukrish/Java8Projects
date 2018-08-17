d91ea75b1122cd71cb70b1a856f222e7d0b1e338
KP Developer <jijukrisnnan@hotmail.com>
2018-08-17 17:05:59 +1000

    This is the first commit for an environment management application developed as Rest API

---
+++ EnvManagement/src/org/bestonjava/restfulapi/app/rest/domain/common/Status.java
@@ -0,0 +1,6 @@
+package org.bestonjava.restfulapi.app.rest.domain.common;
+
+public class Status {
+	public static String ACTIVE="Active";
+	public static String INACTIVE="Inactive";
+}
---
+++ EnvManagement/src/org/bestonjava/restfulapi/dao/ConfigurationDB.java
@@ -0,0 +1,41 @@
+package org.bestonjava.restfulapi.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.bestonjava.restfulapi.app.rest.domain.Configuration;
+import org.jboss.resteasy.annotations.Status;
+ 
+public class ConfigurationDB {
+    private static Map<Integer, Configuration> configurationDB = new ConcurrentHashMap<Integer, Configuration>();
+    private static AtomicInteger idCounter = new AtomicInteger();
+     
+    public static Integer createConfiguration(String content, String status){
+        Configuration c = new Configuration();
+        c.setId(idCounter.incrementAndGet());
+        c.setContent(content);
+        c.setStatus(status);
+        configurationDB.put(c.getId(), c);
+         
+        return c.getId();
+    }
+     
+    public static Configuration getConfiguration(Integer id){
+        return configurationDB.get(id);
+    }
+     
+    public static List<Configuration> getAllConfigurations(){
+        return new ArrayList<Configuration>(configurationDB.values());
+    }
+     
+    public static Configuration removeConfiguration(Integer id){
+        return configurationDB.remove(id);
+    }
+     
+    public static Configuration updateConfiguration(Integer id, Configuration c){
+        return configurationDB.put(id, c);
+    }
+}
